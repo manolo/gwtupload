86e4a9e86bb5ee3dbd603a8ff48ad42cf8c27f9f
Manolo Carrasco <manolo@apache.org>
2011-07-12 07:36:46 +0000

    Fixes issue104
    git-svn-id: https://gwtupload.googlecode.com/svn/trunk/GwtUpload@931 189a91c2-4759-11de-acc5-7fb027fc3da9

--- core/src/main/java/gwtupload/client/DecoratedFileUpload.java
+++ core/src/main/java/gwtupload/client/DecoratedFileUpload.java
@@ -234,7 +234,7 @@ public class DecoratedFileUpload extends Composite implements HasText, HasName, 
     public void init(AbsolutePanel container, FileUploadWithMouseEvents input) {
       super.init(container, input);
       container.add(input, 500, 500);
-      DOM.setStyleAttribute(container.getElement(), "cssFloat", "left");
+      DOM.setStyleAttribute(container.getElement(), "position", "absolute");
       DOM.setStyleAttribute(container.getElement(), "display", "inline");
       DOM.setStyleAttribute(input.getElement(), "top", "-300px");
       DOM.setStyleAttribute(input.getElement(), "left", "-300px");
