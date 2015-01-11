0f213dcd20107446489262551f15abbe2d0113fe
Manolo Carrasco <manolo@apache.org>
2015-01-11 14:29:38 +0100

    Fixes in php script: make it CORS and multiple

--- jsupload/src/main/java/jsupload/public/jsupload.php
+++ jsupload/src/main/java/jsupload/public/jsupload.php
@@ -29,6 +29,9 @@
 ## *   you have to add the following section into php.ini:
 ## *   {{{
 ## *    [APC]
+## *    ....
+## *    extension=apc.so
+## *    [APC]
 ## *    apc.enabled=1
 ## *    apc.shm_segments=1
 ## *    apc.shm_size=64
@@ -37,6 +40,11 @@
 ## *    apc.rfc1867="1"
 ## *    apc.rfc1867_freq="0"
 ## *   }}}
+## *   Previously you must install apc extension in your system, normally installing
+## *    php-devel php-pear pcre-devel
+## *   And running
+## *    pecl install apc
+## *   But you might read the apc documentation.
 ## * # *Client side*:
 ## *   In the client you have to add a hidden widget to the uploader in the first position.
 ## *   * Gwt-upload
@@ -54,11 +62,11 @@
 ## * # *Configuration*: At the top of the script there is a customizable variable.
 ## *   * $uploaddir: is the prefix used in the path location to store the data received. By default
 ## *                 it is set to "/tmp/php_upload/", normally you have to change it to match the path
-## *                 configured in your application. It is strongly recommended that this directory 
+## *                 configured in your application. It is strongly recommended that this directory
 ## *                 should be created apart from "/tmp", in a non-httpd accessible path.
 ## * # *Integration*:
 ## *   * The files are stored in the folder $tmp_dir/xxxx where xxxx is the session id (cookie PHPSESSID).
-## *   * For each file received, it stores a file /tmp/uploader/xxxx/yyyy.info, 
+## *   * For each file received, it stores a file /tmp/uploader/xxxx/yyyy.info,
 ## *     where yyyy is the name of the element, with the original filename and its content-type.
 ## *   * The content of the file in the file will be in the /tmp/uploader/xxxx/yyyy.bin file.
 ## *   * The application must create, handle, and clean $tmp_dir files.
@@ -86,6 +94,21 @@ function writeResponse($msg, $post) {
   die($xml);
 }
 
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  exit(0);
+}
+
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
   # Get is used to request percent while the file is uploaded.
   # It is used also to remove the uploaded file, get it and cancel.
@@ -104,21 +127,43 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
       $total = (int)$status['total'];
       $percent = round($done/$total*100);
 
-      $resp = sprintf("<percent>%d</percent><currentBytes>%d</currentBytes><totalBytes>%d</totalBytes>", 
+      $resp = sprintf("<percent>%d</percent><currentBytes>%d</currentBytes><totalBytes>%d</totalBytes>",
               $percent, $done, $total);
       if($done==$total) $resp .= "<finished>ok</finished>";
         writeResponse($resp, 0);
-      } else {
-        writeResponse("<error>" . $_GET['filename'] . "</error>", 0);
+    } else {
+      $name = $_GET['filename'];
+      $file = $uploaddir . $name;
+      $files = array();
+      if (file_exists($file . ".bin")) array_push($files, $name);
+      for ($cont = 0; file_exists($file . "-" . $cont . ".bin") ; $cont ++) {
+        array_push($files, $name . "-" . $cont);
       }
+      if (sizeof($files) > 0) {
+        $msg = " <message>$version</message><files>\n";
+        foreach($files as $field) {
+          $lines = file ($uploaddir . $field . ".info");
+          $name = chop($lines[0]);
+          $type = chop($lines[1]);
+          $size = chop($lines[2]);
+          $msg  .= " <file>\n  <field>$field</field>\n  <name>$name</name> \n  <ctype>$type</ctype>\n  <size>$size</size>\n </file>";
+        }
+        $msg .= "</files>\n<finished>ok</finished>";
+        writeResponse($msg, 0);
+      } else {
+        writeResponse("<error>" . $file . "</error>", 0);
+      }
+    }
   } else if(isset($_GET['new_session'])) {
     if (!file_exists($uploaddir))
-      mkdir($uploaddir);
+      mkdir($uploaddir, 0700, true);
     writeResponse("<session>ok</session>", 0);
   } else if(isset($_GET['show'])){
-    $file = $uploaddir . $_GET['show'] . ".bin";
-    $info = $uploaddir . $_GET['show'] . ".info";
-    if (file_exists($info)) {  
+    $file = $uploaddir . $_GET['show'];
+    if (!file_exists($file . ".bin")) $file .= "-0";
+    $file = $file . ".bin";
+    $info = $file . ".info";
+    if (file_exists($info)) {
       $lines = file($info);
       header('Content-Type: ' . $lines[1]);
     }
@@ -135,9 +180,9 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     writeResponse("<deleted>true</deleted>", 0);
   } else if(isset($_GET['cancel'])) {
     $_SESSION['canceled'] = true;
-    writeResponse("<canceled>true</canceled>", 0); 
+    writeResponse("<canceled>true</canceled>", 0);
   } else {
-    writeResponse("<error>no parameter</error>", 0); 
+    writeResponse("<error>no parameter</error>", 0);
   }
 } else { //POST
   # Post is used to receive the files and move them to the user's
@@ -147,15 +192,18 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 
   if(!$_POST['APC_UPLOAD_PROGRESS']) {
     writeResponse("<error>You have not sent the APC_UPLOAD_PROGRESS parameter.</error>", 1);
+    exit;
   }
   $key = $_POST['APC_UPLOAD_PROGRESS'];
 
   if(!$_FILES[$key]) {
     writeResponse("<error>You have sent an incorrect APC_UPLOAD_PROGRESS key:" . $key . "</error>", 1);
+    exit;
   }
 
-  $fld = preg_replace("\[\]$", "", $key);
   $cnt = 0;
+  $fld = str_replace("[]", "", $key);
+  $msg = "<message>\n<![CDATA[\n$version\n]]>\n</message>\n<files>\n";
   foreach ($_FILES[$key]['tmp_name'] as $tmpfile) {
     $size = $_FILES[$key]['size'][$cnt];
     $type = $_FILES[$key]['type'][$cnt];
@@ -164,20 +212,14 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $uploadfile = $uploaddir . $field . ".bin";
     $uploadinfo = $uploaddir . $field . ".info";
     if (move_uploaded_file($tmpfile, $uploadfile)) {
-      $servermessage = $version;
-      $msg  = " <file>\n  <field>$field</field>\n  <name>$name</name>"
-            . "\n  <size>$size</size>\n  <ctype>$type</ctype>"
-            . "\n  <message>\n<![CDATA[\n$servermessage\n]]>\n  </message>\n </file>";
-
+      $msg  .= " <file>\n  <field>$field</field>\n  <name>$name</name> \n  <ctype>$type</ctype>\n  <size>$size</size>\n </file>";
       $fh = fopen($uploadinfo, 'w');
-      fwrite($fh, $name . "\n" . $type . "\n");    
+      fwrite($fh, $name . "\n" . $type . "\n" . $size . "\n");
       fclose($fh);
-
-      writeResponse($msg, 1);
-    } else {
-      writeResponse("<error>Unable to move: " . $tmpfile . " to: " . $uploadfile . "</error>", 1);
     }
   }
+  $msg .= "</files>\n<finished>ok</finished>";
+  writeResponse($msg, 1);
 }
 
 ?>
